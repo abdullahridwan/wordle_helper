87f863eb6cc0b61aa7c5413073c5f79055adc67d
Abdullah Ridwan <abdullahridwan73@icloud.com>
2022-02-17 23:51:32 -0500

    deleted red alert

--- app/from_wordle.py
+++ app/from_wordle.py
@@ -1,7 +1,2 @@
-all_words = open("words.txt", "r")
-all_words_content = all_words.read().split("\n")
 
-
-all_allowed = open("allowed.txt", "r")
-all_allowed_content = all_allowed.read().split("\n")
 
